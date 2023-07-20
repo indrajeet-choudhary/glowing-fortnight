a898821aa5e3460ad3b947d751cfaec59ee6e81c
Indrajeet <indrajeet.choudhary@yahoo.com>
2023-07-20 09:22:54 +0530

    Project Added

---
+++ Appointment/Register.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="Appointment.Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Appointment Scheduler
+    </title>
+    <script type="text/Javascript">
+        function validate() {
+            var pass1 = document.getElementById("txtpass").value;
+            var pass2 = document.getElementById("txtpass2").value;
+            if (pass2 != pass1) {
+                alert("Please enter same Password as above");
+            }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblname" runat="server" Text="Name"></asp:Label>
+            <asp:TextBox ID="txtname" runat="server"></asp:TextBox><br />
+            <asp:Label ID="lblemail" runat="server" Text="Mail Id"></asp:Label>
+            <asp:TextBox ID="txtemail" runat="server"></asp:TextBox><br />
+            <asp:Label ID="lblpass" runat="server" Text="Create Password"></asp:Label>
+            <asp:TextBox ID="txtpass" runat="server"></asp:TextBox><br />
+            <asp:Label ID="lblpass2" runat="server" Text="Confirm Password"></asp:Label>
+            <asp:TextBox ID="txtpass2" runat="server"></asp:TextBox><br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+            <asp:Button ID="btncancel" runat="server" Text="Cancel" /><br />
+            <asp:Label ID="Message" runat="server" ></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Appointment/Register.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Appointment
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        string name;
+        string email;
+        string password;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+            }
+            catch (Exception error)
+            {
+                Response.Write("Hello" + error.Message);
+            }
+
+        }
+        protected void btnSubmit_Click(Object sender, EventArgs e)
+        {
+            name = txtname.Text;
+            email = txtemail.Text;
+            password = txtpass2.Text;
+            try
+            {
+                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnect"].ConnectionString);
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "CREATE TABLE [dbo].[register](" +
+                    "[Userid] INT IDENTITY(1001, 1) NOT NULL," +
+                    "[Name] VARCHAR(50) NOT NULL," +
+                    "[Mail] VARCHAR(50) NOT NULL," +
+                    "[Password] VARCHAR(16) NOT NULL," +
+                    "PRIMARY KEY CLUSTERED([Userid] ASC));";
+                cmd.CommandText = "insert into register values ('" + name + "','" + email + "','" + password + "')";
+                cmd.ExecuteNonQuery();
+                Message.Text = "Insert Success";
+            }
+            catch (Exception error)
+            {
+                Message.Text = "Insert declined:" + error;
+            }
+        }
+        protected void btnCancel_Click(Object sender, EventArgs e)
+        {
+
+        }
+    }
+}
